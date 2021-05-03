5163ab3c1f8513f76d5b5c0ba87ef6bc5dadf976
Asher623 <69222787+Asher623@users.noreply.github.com>
2021-05-02 21:21:40 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,4 @@
-/************************************************************************ *********
-* WEB422 – Assignment 06
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students. *
-* Name: __Ivan Anferov____________________ Student ID: ____130909195__________ Date: __03/31/2021______________
-* Online Link: _______________________________________________________________
-* ********************************************************************************/
+
 import { Component, OnInit } from '@angular/core';
 import { Router, Event, NavigationStart } from '@angular/router';
 import { AuthService } from './auth.service';
